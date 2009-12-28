6c5b6e8ff103e504012bea49be77f57e228b9c8e
Doug Latornell <djl@douglatornell.ca>
2009-12-27 22:41:41 -0800

    Set up for deployment on webfaction.

---
+++ rsync2wf.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# rsync the randopony Django app to WebFaction
+rsync -avz \
+    --exclude=.hg* --exclude=*.pyc --exclude=*.db --exclude=rsync2wf.sh \
+    --exclude=settings.py \
+    $PWD webfaction:webapps/randopony/
