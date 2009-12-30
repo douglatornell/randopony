5bb7595d22e989fe97dc54e834e61662b0419123
Doug Latornell <djl@douglatornell.ca>
2009-12-30 15:58:47 -0800

    Added URL mapping so that `/` maps to `/register/` until there are more apps.

--- urls.py
+++ urls.py
@@ -10,6 +10,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Resolve '/' to the register app until there are more apps
+    (r'', include('randopony.register.urls')),
+    # Brevet pre-registration
     (r'^register/', include('randopony.register.urls')),
+    # Django auto-generated site admin
     (r'^admin/', include(admin.site.urls)),
 )
