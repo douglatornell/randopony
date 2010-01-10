2fdf1586784cbc8117b3f9912c79e6a792482f74
Doug Latornell <djl@douglatornell.ca>
2010-01-09 18:58:11 -0800

    Changed SECRET_KEY to read from a file.

--- rsync2wf.sh
+++ rsync2wf.sh
@@ -3,5 +3,5 @@
 # rsync the randopony Django app to WebFaction
 rsync -avz \
     --exclude=.hg* --exclude=*.pyc --exclude=*.db --exclude=rsync2wf.sh \
-    --exclude=settings.py --exclude=.DS_Store \
+    --exclude=settings.py --exclude=.secret_key --exclude=.DS_Store \
     $PWD webfaction:webapps/randopony/
