41d9fe1721d2d7d784e769b9e054dda64dd36832
Doug Latornell <djl@douglatornell.ca>
2010-07-12 19:45:11 -0700

    Exclude getwfdb.sh from rsync to webfaction.

--- rsync2wf.sh
+++ rsync2wf.sh
@@ -4,5 +4,5 @@
 rsync -ahvz \
     --exclude=.hg* --exclude=*.pyc --exclude=*.db --exclude=rsync2wf.sh \
     --exclude=settings.py --exclude=.secret_key --exclude=.DS_Store \
-    --exclude=*.aside --exclude=*.backup \
+    --exclude=*.aside --exclude=*.backup --exclude=getwfdb.sh \
     $PWD webfaction:webapps/randopony/
