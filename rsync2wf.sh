457307edb7240e1f9c5c62fa8abee5a402983cfd
Doug Latornell <djl@douglatornell.ca>
2010-10-31 18:29:07 -0700

    Replace custom deployment mgmt scripts with Makefile.

--- rsync2wf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# rsync the randopony Django app to WebFaction
-rsync -ahvz \
-    --exclude=.hg* --exclude=*.pyc --exclude=*.db --exclude=rsync2wf.sh \
-    --exclude=settings.py --exclude=.secret_key --exclude=.DS_Store \
-    --exclude=*.aside --exclude=*.backup --exclude=getwfdb.sh \
-    $PWD webfaction:webapps/randopony/
