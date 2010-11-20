15f4cf906eb2b1a227dfb8a236dfefea59c875e1
Doug Latornell <djl@douglatornell.ca>
2010-11-20 10:22:52 -0800

    Change to reference views as imports instead of via strings.

--- register/urls.py
+++ register/urls.py
@@ -4,9 +4,11 @@
 # Django:
 from django.conf import settings
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import direct_to_template
 # Application:
 import randopony.register.helpers as h
 import randopony.register.models as model
+import randopony.register.views as views
 
 
 REGIONS = '(?i)%s' % '|'.join(model.REGIONS.keys())
@@ -15,25 +17,26 @@ DAYS = '(0*)[1-9]|[12][0-9]|3[01]'
 MONTHS = '(?i)Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec'
 YEAR = '20\d\d'
 
-urlpatterns = patterns('randopony.register.views',
+urlpatterns = patterns('',
     # Register app home page
-    url(r'^$', 'home', name='home'),
+    url(r'^$', views.home, name='home'),
                        
     # Region brevet pages
-    (r'^(?P<region>({0}))-events/$'.format(REGIONS), 'region_brevets'),
+    (r'^(?P<region>({0}))-events/$'.format(REGIONS), views.region_brevets),
+                       
     # Brevet page with rider list
     (r'^(?P<region>({regions}))(?P<event>{events})/'
       '(?P<date>({days})({months})({year}))/$'
       .format(regions=REGIONS, events=EVENTS,
               days=DAYS, months=MONTHS, year=YEAR),
-      'brevet'),
+      views.brevet),
                        
     # Brevet page with rider pre-registration confirmation message
     (r'^(?P<region>({regions}))(?P<event>{events})/'
       '(?P<date>({days})({months})({year}))/(?P<rider_id>\d+)/$'
       .format(regions=REGIONS, events=EVENTS,
               days=DAYS, months=MONTHS, year=YEAR),
-      'brevet'),
+      views.brevet),
                        
     # Brevet page with rider pre-registration duplication message
     (r'^(?P<region>({regions}))(?P<event>{events})/'
@@ -41,26 +44,26 @@ urlpatterns = patterns('randopony.register.views',
       '(?P<rider_id>\d+)/duplicate/$'
       .format(regions=REGIONS, events=EVENTS,
               days=DAYS, months=MONTHS, year=YEAR),
-      'brevet'),
+      views.brevet),
                        
     # Rider pre-registration form page
     (r'^(?P<region>({regions}))(?P<event>{events})/'
       '(?P<date>({days})({months})({year}))/form/$'
       .format(regions=REGIONS, events=EVENTS,
               days=DAYS, months=MONTHS, year=YEAR),
-      'registration_form'),
+      views.registration_form),
 )
 
-urlpatterns += patterns('django.views.generic.simple',
+urlpatterns += patterns('',
     # What's up with the pony page
-    (r'^about_pony/$', 'direct_to_template',
+    (r'^about_pony/$', direct_to_template,
      {
          'template': 'derived/about/about-pony.html'
      },
      'about_pony'),
 
     # Info for brevet organizers page
-    (r'^organizer_info/$', 'direct_to_template',
+    (r'^organizer_info/$', direct_to_template,
      {
          'template': 'derived/about/about-pony.html',
          'extra_context': {
