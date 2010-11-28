83cf0f70566edf708944af9d9722c07aefff0ed0
Doug Latornell <djl@douglatornell.ca>
2010-11-27 17:10:56 -0800

    Define Brevet and ClubEvent model classes that inherit from BaseEvent.

--- register/tests/test_models.py
+++ register/tests/test_models.py
@@ -6,19 +6,41 @@ import unittest
 from datetime import date
 
 
-class TestBaseEvent(unittest.TestCase):
-    """Unit tests for BaseEvent model object.
+class TestBrevet(unittest.TestCase):
+    """Unit tests for Brevet model object.
     """
     def _get_target_class(self):
-        from randopony.register.models import BaseEvent
-        return BaseEvent
+        from randopony.register.models import Brevet
+        return Brevet
 
 
     def _make_one(self, *args, **kwargs):
         return self._get_target_class()(*args, **kwargs)
 
 
-    def test_club_event_unicode(self):
+    def test_unicode(self):
+        """__unicode__ returns {region}{event} {date} for brevets
+        """
+        today = date.today()
+        event = self._make_one(
+            region='LM', event='200', date=today)
+        self.assertEqual(
+            unicode(event), u'LM200 {0}'.format(today.strftime('%d-%b-%Y')))
+
+
+class TestClubEvent(unittest.TestCase):
+    """Unit tests for ClubEvent model object.
+    """
+    def _get_target_class(self):
+        from randopony.register.models import ClubEvent
+        return ClubEvent
+
+
+    def _make_one(self, *args, **kwargs):
+        return self._get_target_class()(*args, **kwargs)
+
+
+    def test_unicode(self):
         """__unicode__ returns {event} {date} for club events
         """
         today = date.today()
@@ -26,13 +48,3 @@ class TestBaseEvent(unittest.TestCase):
             region='Club', event='AGM', date=today)
         self.assertEqual(
             unicode(event), u'AGM {0}'.format(today.strftime('%d-%b-%Y')))
-
-
-    def test_riding_event_unicode(self):
-        """__unicode__ returns {region}{event} {date} for riding events
-        """
-        today = date.today()
-        event = self._make_one(
-            region='LM', event='200', date=today)
-        self.assertEqual(
-            unicode(event), u'LM200 {0}'.format(today.strftime('%d-%b-%Y')))
