0c3e18297a446192641a5f202a74860adb79391e
Doug Latornell <djl@douglatornell.ca>
2011-02-25 11:07:55 -0800

    Add code to put link on pasture home page when there are populaires.

--- pasture/views.py
+++ pasture/views.py
@@ -1,6 +1,7 @@
 """View functions for RandoPony top level (pasture) app.
 
 """
+from __future__ import absolute_import
 # Standard library:
 from datetime import datetime
 from datetime import timedelta
@@ -9,8 +10,9 @@ from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 # RandoPony:
-from randopony.register.helpers import email2words
-from randopony.register.models import Brevet
+from ..populaires.models import Populaire
+from ..register.helpers import email2words
+from ..register.models import Brevet
 
 
 def home(request):
@@ -20,9 +22,12 @@ def home(request):
     seven_days_ago = datetime.today().date() - timedelta(days=7)
     brevet_list = Brevet.objects.exclude(
         date__lt=(seven_days_ago))
+    populaire_list = Populaire.objects.exclude(
+        date__lt=(seven_days_ago))
     context = RequestContext(request, {
         'admin_email': email2words(settings.ADMINS[0][1]),
         'brevets': brevet_list,
+        'populaires': populaire_list,
     })
     response = render_to_response('pasture/templates/derived/home.html', context)
     return response
