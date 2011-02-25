a11363b3f18310c48b7a41c4385177adbea47e7b
Doug Latornell <djl@douglatornell.ca>
2011-02-25 12:08:56 -0800

    Add Google Docs rider list creation to populaires admin functions.

--- populaires/admin.py
+++ populaires/admin.py
@@ -10,8 +10,13 @@ from django.core import mail
 from django.core.urlresolvers import reverse
 from django.core.validators import validate_email
 from django.template.loader import render_to_string
+# Google Docs:
+from gdata.docs.client import DocsClient
 # RandoPony:
 from .models import Populaire
+from ..helpers import get_rider_list_template
+from ..helpers import google_docs_login
+from ..helpers import share_rider_list_publicly
         
 
 class CustomBrevetAdminForm(forms.ModelForm):
@@ -45,10 +50,43 @@ class PopulaireAdmin(admin.ModelAdmin):
                                    .split()}),
     ]
     actions = [
-#         'create_rider_list_spreadsheet',
+        'create_rider_list_spreadsheet',
         'notify_webmaster',
 #         'notify_populaire_organizer',
     ]
+
+    
+    def create_rider_list_spreadsheet(self, request, queryset):
+        """Create a Google Docs rider list spreadsheet from the rider
+        list template for the populaire(s) in the queryset.
+        
+        Handler for create_rider_list_spreadsheet admin action.
+        """
+        client = google_docs_login(DocsClient)
+        docs_count = 0
+        pop_count = queryset.count()
+        for pop in queryset:
+            if not pop.google_doc_id:
+                template = get_rider_list_template(
+                    'Populaire Rider List Template', client)
+                created_doc = client.copy(template, unicode(pop))
+                share_rider_list_publicly(created_doc, client)
+                pop.google_doc_id = created_doc.resource_id.text
+                pop.save()
+                docs_count += 1
+        if docs_count == 0:
+            msg = 'No rider lists created!'
+        elif docs_count == 1:
+            msg = 'Rider list created for 1 populaire.'
+        else:
+            msg = 'Rider lists created for {0} populaires.'.format(docs_count)
+        diff = pop_count - docs_count
+        if diff:
+            if diff == 1:
+                msg += ' Populaire already had a rider list.'
+            else:
+                msg += ' {0} populaires already had rider lists.'.format(diff)
+        self.message_user(request, msg)
 
 
     def notify_webmaster(self, request, queryset):
