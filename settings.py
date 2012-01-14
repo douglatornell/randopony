30f55b4f6356794dc84e0cd29185d2895c73119b
Doug Latornell <djl@douglatornell.ca>
2012-01-14 15:40:22 -0800

    Clean up settings file.

--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
-"""Django settings for development instance of RandoPony site.
+"""Django settings for RandoPony project.
 """
 from os import path
-
 
 project_path = path.dirname(__file__)
 
@@ -23,44 +22,28 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/Vancouver'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-ca'
 
 SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = path.join(project_path, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = 'http://localhost/~doug/django_media/randopony/'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
 ADMIN_MEDIA_PREFIX = '/media/'
 
-# Make this unique, and don't share it with anybody.
 SECRET_KEY = open(path.join(project_path, '.secret_key'), 'r').read()
 
-# List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.filesystem.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -73,9 +56,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'randopony.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    # Always use absolute paths.
     path.join(project_path, 'media/register/templates'),
     path.join(project_path, 'media/pasture/templates'),
     path.join(project_path, 'media/populaires/templates'),
