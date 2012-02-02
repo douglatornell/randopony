00f7679f6dbd56568f13871dbdb0ca9e6a8a8c71
Doug Latornell <djl@douglatornell.ca>
2012-02-01 21:59:53 -0800

    Change Google spreadsheet update & emails upon registration into celery tasks.

--- register/views.py
+++ register/views.py
@@ -6,26 +6,22 @@ from datetime import datetime
 from datetime import timedelta
 # Django:
 from django.conf import settings
-from django.core import mail
-from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
 from django.template import RequestContext
-from django.template.loader import render_to_string
-# Google Docs:
-from gdata.spreadsheet.service import SpreadsheetsService
 # RandoPony:
 from .models import Brevet
 from .models import BrevetRider
 from .models import REGIONS
 from .models import RiderForm
 from .models import RiderFormWithoutInfoQuestion
+from .tasks import email_to_rider
+from .tasks import email_to_organizer
+from .tasks import update_google_spreadsheet
 from ..pasture.helpers import email2words
-from ..pasture.helpers import google_docs_login
-from ..pasture.models import EmailAddress
 from ..pasture.models import Link
 
 
@@ -109,7 +105,7 @@ def brevet(request, region, event, date, rider_id=None):
             brevet__date=brevet.date)
         try:
             rider = BrevetRider.objects.get(pk=int(rider_id))
-            rider_email = email2words(rider.email)
+            rider_email = rider.email
         except (TypeError, BrevetRider.DoesNotExist, AttributeError):
             rider = rider_email = None
         template = 'register/brevet.html'
@@ -198,8 +194,7 @@ def brevet_rider_emails(request, region, event, date, uuid):
 def _process_registration(brevet, rider, request):
     """Process rider pre-registration for brevet.
     """
-    brevet_page = 'register/{0.region}{0.event}/{1}'.format(
-        brevet, brevet.date.strftime('%d%b%Y'))
+    brevet_page = 'register/{0.region}{0.event}/{0.date:%d%b%Y}'.format(brevet)
     # Check for duplicate registration
     try:
         check_rider = BrevetRider.objects.get(
@@ -207,103 +202,15 @@ def _process_registration(brevet, rider, request):
             email=rider.email, brevet=brevet)
         # Redirect to brevet page with duplicate flag to
         # trigger appropriate flash message
-        return '/{0}/{1:d}/duplicate/'.format(brevet_page, check_rider.id)
+        return '/{0}/{1:d}/duplicate/'.format(brevet_page, check_rider.pk)
     except BrevetRider.DoesNotExist:
         # Save new rider pre-registration and send emails to
         # rider and brevet organizer
         rider.save()
-        _update_google_spreadsheet(brevet)
+        update_google_spreadsheet.delay(brevet.pk)
         host = request.get_host()
-        _email_to_rider(brevet, rider, host)
-        _email_to_organizer(brevet, rider, host)
+        email_to_rider.delay(brevet.pk, rider.pk, host)
+        email_to_organizer.delay(brevet.pk, rider.pk, host)
         # Redirect to brevet page with rider record id to
         # trigger registration confirmation flash message
-        return '/{0}/{1:d}/'.format(brevet_page, rider.id)
-
-
-def _update_google_spreadsheet(brevet):
-    """Update the rider list spreadsheet on Google docs, preserving
-    the list's sorted by last name order.
-    """
-    client = google_docs_login(SpreadsheetsService)
-    key = brevet.google_doc_id.split(':')[1]
-    spreadsheet_list = client.GetListFeed(key)
-    spreadsheet_rows = len(spreadsheet_list.entry)
-    rider_list = BrevetRider.objects.filter(
-        brevet__region=brevet.region, brevet__event=brevet.event,
-        brevet__date=brevet.date)
-    # Update the rows already in the spreadsheet
-    for row, rider in enumerate(rider_list[:spreadsheet_rows]):
-        rider_number = row + 1
-        new_row_data = _make_spreadsheet_row_dict(rider_number, rider)
-        client.UpdateRow(spreadsheet_list.entry[row], new_row_data)
-        client.UpdateCell(rider_number + 1, 5, rider.info_answer, key)
-    # Add remaining rows
-    for row, rider in enumerate(rider_list[spreadsheet_rows:]):
-        rider_number = spreadsheet_rows + row + 1
-        row_data = _make_spreadsheet_row_dict(rider_number, rider)
-        client.InsertRow(row_data, key)
-        client.UpdateCell(rider_number + 1, 5, rider.info_answer, key)
-
-
-def _make_spreadsheet_row_dict(rider_number, rider):
-        row_data = {
-            'ridernumber': str(rider_number),
-            'lastname': rider.last_name,
-            'firstname': rider.first_name,
-            'clubmember': 'Y' if rider.club_member else 'N',
-        }
-        return row_data
-
-
-def _email_to_rider(brevet, rider, host):
-    """Send pre-registration confirmation email to rider.
-    """
-    brevet_page = reverse(
-        'register:brevet',
-        args=(brevet.region, brevet.event, brevet.date.strftime('%d%b%Y')))
-    brevet_page_url = 'http://{0}{1}'.format(host, brevet_page)
-    event_waiver_url = Link.objects.get(key='event_waiver_url').url
-    membership_form_url = Link.objects.get(key='membership_form_url').url
-    from_randopony = EmailAddress.objects.get(key='from_randopony').email
-    email = mail.EmailMessage(
-        subject='Pre-registration Confirmation for {0} Brevet'
-                .format(brevet),
-        body=render_to_string(
-            'register/email/to_rider.txt',
-            {'brevet': brevet,
-             'rider': rider,
-             'brevet_page_url': brevet_page_url,
-             'event_waiver_url': event_waiver_url,
-             'membership_form_url': membership_form_url,
-             }),
-        from_email=brevet.organizer_email,
-        to=[rider.email],
-        headers={
-            'Sender': from_randopony,
-            'Reply-To': brevet.organizer_email}
-    )
-    email.send()
-
-
-def _email_to_organizer(brevet, rider, host):
-    """Send rider pre-registration notification email to event organizer(s).
-    """
-    brevet_page = reverse(
-        'register:brevet',
-        args=(brevet.region, brevet.event, brevet.date.strftime('%d%b%Y')))
-    brevet_page_url = 'http://{0}{1}'.format(host, brevet_page)
-    from_randopony = EmailAddress.objects.get(key='from_randopony').email
-    email = mail.EmailMessage(
-        subject='{0} has Pre-registered for the {1}'
-                .format(rider.full_name, brevet),
-        body=render_to_string(
-            'register/email/to_organizer.txt',
-            {'brevet': brevet,
-             'rider': rider,
-             'brevet_page_url': brevet_page_url,
-             'admin_email': settings.ADMINS[0][1]}),
-        from_email=from_randopony,
-        to=[addr.strip() for addr in brevet.organizer_email.split(',')]
-    )
-    email.send()
+        return '/{0}/{1:d}/'.format(brevet_page, rider.pk)
