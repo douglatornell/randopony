535d8d897471aacbd03ec8d663db0840dca796a4
Doug Latornell <djl@douglatornell.ca>
2012-02-11 12:44:47 -0800

    Change populaires Google spreadsheet update & emails upon reg to celery tasks.

--- populaires/tests/__init__.py
+++ populaires/tests/__init__.py
@@ -1,3 +1,4 @@
 from .test_admin import *               # NOQA
 from .test_models import *              # NOQA
+from .test_tasks import *               # NOQA
 from .test_views import *               # NOQA
--- populaires/views.py
+++ populaires/views.py
@@ -5,7 +5,6 @@ from datetime import datetime
 from datetime import timedelta
 # Django:
 from django.conf import settings
-from django.core import mail
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from django.http import HttpResponse
@@ -13,16 +12,14 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.template.loader import render_to_string
-# Google Docs:
-from gdata.spreadsheet.service import SpreadsheetsService
 # RandoPony:
 from .models import Populaire
 from .models import Rider
 from .models import RiderForm
+from .tasks import update_google_spreadsheet
+from .tasks import email_to_rider
+from .tasks import email_to_organizer
 from ..pasture.helpers import email2words
-from ..pasture.helpers import google_docs_login
-from ..pasture.models import EmailAddress
 
 
 def populaires_list(request):
@@ -127,93 +124,14 @@ def _process_registration(populaire, rider, request):
         # Save new rider pre-registration and send emails to
         # rider and brevet organizer
         rider.save()
-        _update_google_spreadsheet(populaire)
-        _email_to_rider(populaire, rider, request.get_host())
-        _email_to_organizer(populaire, rider, request.get_host())
+        update_google_spreadsheet.delay(populaire.pk)
+        email_to_rider.delay(populaire.pk, rider.pk, request.get_host())
+        email_to_organizer.delay(populaire.pk, rider.pk, request.get_host())
         url = reverse(
             'populaires:prereg-confirm',
             args=(populaire.short_name, populaire.date.strftime('%d%b%Y'),
                   rider.id))
     return url
-
-
-def _update_google_spreadsheet(populaire):
-    """Update the rider list spreadsheet on Google docs, preserving
-    the list's sorted by last name order.
-    """
-    client = google_docs_login(SpreadsheetsService)
-    key = populaire.google_doc_id.split(':')[1]
-    spreadsheet_list = client.GetListFeed(key)
-    spreadsheet_rows = len(spreadsheet_list.entry)
-    rider_list = Rider.objects.filter(
-        populaire__short_name=populaire.short_name,
-        populaire__date=populaire.date)
-    # Update the rows already in the spreadsheet
-    for row, rider in enumerate(rider_list[:spreadsheet_rows]):
-        rider_number = row + 1
-        new_row_data = _make_spreadsheet_row_dict(rider_number, rider)
-        client.UpdateRow(spreadsheet_list.entry[row], new_row_data)
-    # Add remaining rows
-    for row, rider in enumerate(rider_list[spreadsheet_rows:]):
-        rider_number = spreadsheet_rows + row + 1
-        row_data = _make_spreadsheet_row_dict(rider_number, rider)
-        client.InsertRow(row_data, key)
-
-
-def _make_spreadsheet_row_dict(rider_number, rider):
-        row_data = {
-            'ridernumber': str(rider_number),
-            'lastname': rider.last_name,
-            'firstname': rider.first_name,
-            'distance': str(rider.distance),
-        }
-        return row_data
-
-
-def _email_to_rider(populaire, rider, host):
-    """Send pre-registration confirmation email to rider.
-    """
-    pop_page = reverse(
-        'populaires:populaire',
-        args=(populaire.short_name, populaire.date.strftime('%d%b%Y')))
-    pop_page_url = 'http://{0}{1}'.format(host, pop_page)
-    from_randopony = EmailAddress.objects.get(key='from_randopony').email
-    email = mail.EmailMessage(
-        subject='Pre-registration Confirmation for {0}'.format(populaire),
-        body=render_to_string(
-            'populaires/email/to_rider.txt',
-            {'populaire': populaire,
-             'pop_page_url': pop_page_url}),
-        from_email=populaire.organizer_email,
-        to=[rider.email],
-        headers={
-            'Sender': from_randopony,
-            'Reply-To': populaire.organizer_email}
-    )
-    email.send()
-
-
-def _email_to_organizer(populaire, rider, host):
-    """Send rider pre-registration notification email to event organizer(s).
-    """
-    pop_page = reverse(
-        'populaires:populaire',
-        args=(populaire.short_name, populaire.date.strftime('%d%b%Y')))
-    pop_page_url = 'http://{0}{1}'.format(host, pop_page)
-    from_randopony = EmailAddress.objects.get(key='from_randopony').email
-    email = mail.EmailMessage(
-        subject='{0} has Pre-registered for the {1}'
-                .format(rider.full_name, populaire),
-        body=render_to_string(
-            'populaires/email/to_organizer.txt',
-            {'populaire': populaire,
-             'rider': rider,
-             'pop_page_url': pop_page_url,
-             'admin_email': settings.ADMINS[0][1]}),
-        from_email=from_randopony,
-        to=[addr.strip() for addr in populaire.organizer_email.split(',')]
-    )
-    email.send()
 
 
 def rider_emails(request, short_name, date, uuid):
